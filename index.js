65409a46ebb00af24e4b3e96bdc8647517f6f439
CurlyFury <nukawanderer@gmail.com>
2021-09-19 13:51:43 +0800

    ejs

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(requestTime)
 app.use(logger)
 
-/* app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+app.get('/', (req, res) => {
+	res.render('index', { title: 'Main page', active: 'main' })
 })
 
 app.get('/features', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'static', 'features.html'))
-}) */
-
-app.get('/download', (req, res) => {
-	res.download(path.resolve(__dirname, 'static', 'index.html'))
+	res.render('features', { title: 'Features page', active: 'features' })
 })
 
 app.listen(PORT, () => {
